31bf9d7d060e18d1b7307415c2f376c8d28ff0c4
Julie <julie.barenne@dsi-ap.com>
2026-02-04 12:01:08 +0100

    SCAFFOLDING (initial) Initialize project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
-module.exports = (() => {
-    const greetings = 'Hello world!'
-    console.log(greetings)
-    return greetings
-})()
+// index.js
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  // Je construis le chemin vers index.html
+  const filePath = path.join(__dirname, 'index.html');
+  
+  // Je lis le fichier et l'envoie au client
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Erreur serveur');
+      return;
+    }
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(content);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Serveur lancé sur http://localhost:3000');
+});
